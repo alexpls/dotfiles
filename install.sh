695a3816225d9dd1da50d080925cae0fc6a27462
Alex Plescan <alexpls@gmail.com>
2017-02-13 14:49:28 +1000

    Update install to install tmux changes too

--- install.sh
+++ install.sh
@@ -4,10 +4,13 @@ BASH_DIR="$(pwd)/bash"
 
 echo "Installing dotfiles..."
 
+# Install bash stuff
 echo "
 source $(pwd)/attach.sh "$(pwd)"
 " >> ~/.bash_profile
-
 source ~/.bash_profile
+
+# Install tmux stuff
+cat "tmux/tmux.conf" >> ~/.tmux.conf
 
 echo "Done!"
