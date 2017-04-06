2c05a2fcfc94428589483ea6bb0c31823d09cf89
Alex Plescan <alexpls@gmail.com>
2017-04-07 09:45:25 +1000

    Some more git configs
    Taken from https://blog.scottnonnenberg.com/better-git-configuration/

--- git/aliases.sh
+++ git/aliases.sh
@@ -2,3 +2,9 @@
 
 # push current branch to remote 'origin', setting upstream automatically
 git config --global alias.pushup 'push -u origin HEAD'
+
+# Stash all files (including untracked)
+git config --global alias.stash-all 'stash save --include-untracked'
+
+# Pretty graphical log
+git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
