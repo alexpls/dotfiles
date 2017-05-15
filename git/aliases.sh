1ecad5ab7f2e08e9bb2c26b59c1182e317d5e3f3
Alex Plescan <alexpls@gmail.com>
2017-05-15 13:27:16 +1000

    Add git alias to list branches ordered by most recent commit

--- git/aliases.sh
+++ git/aliases.sh
@@ -8,3 +8,7 @@ git config --global alias.stash-all 'stash save --include-untracked'
 
 # Pretty graphical log
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
+
+# List branches ordered by most recent commits
+git config --global alias.branchr 'branch --sort=-committerdate'
+git config --global pager.branchr true
