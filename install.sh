91bfcd09453c66edd27caf27b0bd66629da31d10
Alex Plescan <alexpls@gmail.com>
2017-07-03 10:42:21 +1000

    Support systems that use .profile instead of .bash_profile

--- install.sh
+++ install.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+set -e
+
 BASH_DIR="$(pwd)/bash"
+PROFILE_FILE=""
+
+if [[ -e /home/$USER/.bash_profile ]]; then
+  PROFILE_FILE="/home/$USER/.bash_profile"
+elif [[ -e /home/$USER/.profile ]]; then
+  PROFILE_FILE="/home/$USER/.profile"
+else
+  echo "Could not figure out what your profile file is"
+  exit 1
+fi
 
 echo "Installing dotfiles..."
 
 # Install bash stuff
 echo "
 source $(pwd)/attach.sh "$(pwd)"
-" >> ~/.bash_profile
-source ~/.bash_profile
+" >> "$PROFILE_FILE"
+source "$PROFILE_FILE"
 
 # Install tmux stuff
 cat "tmux/tmux.conf" >> ~/.tmux.conf
