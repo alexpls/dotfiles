fec59808cdeecb81512f111d5e273ec01de08355
Alex Plescan <alexpls@fastmail.com>
2024-06-12 17:36:14 +1000

    revert opacity changes

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -8,7 +8,7 @@ local config = wezterm.config_builder()
 config.font = wezterm.font('Berkeley Mono')
 config.font_size = 13
 
-config.window_background_opacity = 0.75
+config.window_background_opacity = 0.95
 config.macos_window_background_blur = 40
 config.window_decorations = "RESIZE | MACOS_FORCE_ENABLE_SHADOW"
 config.window_padding = {
