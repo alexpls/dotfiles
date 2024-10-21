a742c3863a94e854be69686f7cb42790872c5b9a
Alex Plescan <alexpls@fastmail.com>
2024-10-21 18:06:35 +1000

    nvim tabstop and relative linenums

--- nvim/.config/nvim/lua/opts.lua
+++ nvim/.config/nvim/lua/opts.lua
@@ -1,9 +1,15 @@
-vim.opt.number = true     -- show line numbers
-vim.opt.scrolloff = 10    -- num screen lines to show above/below cursor
-vim.opt.undofile = true   -- persist undo history to file
+vim.opt.number = true         -- show line numbers
+vim.opt.relativenumber = true -- relative line numbers
+vim.opt.scrolloff = 10        -- num screen lines to show above/below cursor
+vim.opt.undofile = true       -- persist undo history to file
 vim.opt.signcolumn = "yes"
-vim.opt.updatetime = 1000 -- after this many seconds without input, save swap file
+vim.opt.updatetime = 1000     -- after this many seconds without input, save swap file
 vim.opt.timeoutlen = 300
+
+-- Tabs & spaces
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
 -- Leader key mapping
 vim.g.mapleader = " "
