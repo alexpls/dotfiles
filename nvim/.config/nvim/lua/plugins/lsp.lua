ac24da268ccbe4a4d2b10cc073d7cef298818cfe
Alex Plescan <alexpls@fastmail.com>
2025-01-03 15:39:19 +1000

    nvim set up snippet completion

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -12,33 +12,6 @@ return {
     lazy = false,
     config = true,
   },
-  -- autocompletion
-  {
-    "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-    dependencies = {
-      { "L3MON4D3/LuaSnip" },
-    },
-    config = function()
-      local cmp = require("cmp")
-
-      cmp.setup({
-        sources = {
-          { name = "nvim_lsp" },
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-d>"] = cmp.mapping.scroll_docs(4),
-        }),
-        snippet = {
-          expand = function(args)
-            vim.snippet.expand(args.body)
-          end,
-        },
-      })
-    end
-  },
   -- quickstart configs for nvim lsp
   {
     "neovim/nvim-lspconfig",
