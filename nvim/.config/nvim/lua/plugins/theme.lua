1e5ddcc9e24ac7a42485f31a2906f167a1068d18
Alex Plescan <alexpls@fastmail.com>
2025-02-05 08:37:06 +1000

    nvim tokyonight transparency fix

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -2,17 +2,13 @@ return {
   -- pretty colour scheme
   {
     "folke/tokyonight.nvim",
-    opts = {
-      transparent = true,
-      terminal_colors = true,
-      styles = {
-        sidebars = "dark",
-        floats = "dark",
-      },
-    },
+    lazy = false,
+    priority = 1000,
     config = function()
-      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      require("tokyonight").setup({
+        transparent = true,
+      })
+
       vim.cmd.colorscheme(vim.o.background == "dark" and "tokyonight" or "tokyonight-day")
     end,
   },
