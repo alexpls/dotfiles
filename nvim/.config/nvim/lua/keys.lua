00a6f3dc6852e3cf34d402acb8613b4a0cec1fdf
Alex Plescan <alexpls@fastmail.com>
2025-03-22 13:17:02 +1000

    nvim prefer '+' register when copying to system clipboard
    Since the '+' register works on Linux and macOS, whereas the previously
    used '*' register only seems to work on macOS.

--- nvim/.config/nvim/lua/keys.lua
+++ nvim/.config/nvim/lua/keys.lua
@@ -2,7 +2,7 @@ vim.keymap.set("n", "<C-l>", "<cmd>nohlsearch<CR><C-l>")
 vim.keymap.set("i", "<C-l>", "<cmd>nohlsearch<CR>")
 
 -- Copy to system clipboard
-vim.keymap.set({ "n", "x" }, "<leader>y", '"*y')
+vim.keymap.set({ "n", "x" }, "<leader>y", '"+y')
 
 -- Codeowners actions
 vim.keymap.set("n", "<leader>co", require("codeowners").print)
