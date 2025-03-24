3cb97b903b620bcbec5dd817b22d89dc40749fe3
Alex Plescan <alexpls@fastmail.com>
2025-03-24 18:37:30 +1000

    nvim keybind to find & search sibling files

--- nvim/.config/nvim/lua/plugins/navigation.lua
+++ nvim/.config/nvim/lua/plugins/navigation.lua
@@ -1,3 +1,14 @@
+local cwd_for_telescope = function()
+  local cwd = nil
+  if vim.bo.filetype == "netrw" then
+    cwd = vim.b.netrw_curdir
+  else
+    local current_file = vim.api.nvim_buf_get_name(0)
+    cwd = vim.fn.fnamemodify(current_file, ":h")
+  end
+  return cwd
+end
+
 return {
   -- fuzzy finder
   {
@@ -21,11 +32,55 @@ return {
       },
     },
     keys = {
-      { "<leader>ff", function() require("telescope.builtin").find_files({ find_command = { "rg", "--files", "--hidden", "-g", "!.git" } }) end },
-      { "<leader>fg", function() require("telescope.builtin").git_status() end },
-      { "<leader>fs", function() require("telescope.builtin").live_grep() end },
-      { "<leader>fd", function() require("telescope.builtin").grep_string() end },
-      { "<leader>fb", function() require("telescope.builtin").buffers() end },
+      {
+        "<leader>ff",
+        function()
+          require("telescope.builtin").find_files({
+            find_command = { "rg", "--files", "--hidden", "-g", "!.git" }
+          })
+        end
+      },
+      {
+        -- Find sibling files to the currently open file, or find files in the
+        -- current netrw window if that's open instead.
+        -- Mnemonic: Find siBling Files
+        "<leader>fbf",
+        function()
+          require("telescope.builtin").find_files({
+            find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+            cwd = cwd_for_telescope(),
+          })
+        end
+      },
+      {
+        "<leader>fs",
+        function()
+          require("telescope.builtin").live_grep()
+        end
+      },
+      -- Search sibling files to the currently open file, or search in the
+      -- current netrw window if that's open instead.
+      -- Mnemonic: Find siBling Search (really shoehorning this one...)
+      {
+        "<leader>fbs",
+        function()
+          require("telescope.builtin").live_grep({
+            cwd = cwd_for_telescope(),
+          })
+        end
+      },
+      {
+        "<leader>fd",
+        function()
+          require("telescope.builtin").grep_string()
+        end
+      },
+      {
+        "<leader>fb",
+        function()
+          require("telescope.builtin").buffers()
+        end
+      },
     },
   },
   -- telescope extension, sorts results by most relevant
