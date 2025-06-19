fd2103256b3c80232adea11c6071abbffe5640cc
Alex Plescan <alexpls@fastmail.com>
2025-06-19 21:06:01 +1000

    nvim go snippets

--- nvim/.config/nvim/lua/plugins/autocompletion.lua
+++ nvim/.config/nvim/lua/plugins/autocompletion.lua
@@ -1,67 +1,32 @@
 return {
   -- snippet & LSP autocompletion
   {
-    "hrsh7th/nvim-cmp",
+    "L3MON4D3/LuaSnip",
     event = "InsertEnter",
+    config = function()
+      require("luasnip.loaders.from_snipmate").lazy_load()
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    version = '1.*',
     dependencies = {
       { "L3MON4D3/LuaSnip" },
-      { "saadparwaiz1/cmp_luasnip" },
     },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
-
-      luasnip.config.setup()
-
-      cmp.setup({
-        sources = {
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-Enter>"] = cmp.mapping(function(fallback)
-            -- Confirm the selected entry, or if none is selected
-            -- the first entry in the list.
-            if cmp.visible() and #cmp.get_entries() > 0 then
-              local selected = cmp.get_selected_entry()
-              if not selected then
-                cmp.select_next_item()
-              end
-              cmp.confirm()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-d>"] = cmp.mapping.scroll_docs(4),
-
-          -- Thanks: https://github.com/vimjoyer/nvim-video/blob/main/after/plugin/cmp.lua
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        }),
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-      })
-    end
+    opts = {
+      keymap = {
+        preset = 'default',
+        ['<Enter>'] = { 'accept', 'fallback' },
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+        ['<Tab>'] = { 'select_next', 'fallback' },
+      },
+      completion = { documentation = { auto_show = false } },
+      snippets = { preset = 'luasnip' },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+      },
+      fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+    opts_extend = { "sources.default" }
   },
 }
