16b8b7c45ec88e84fceb7f67f0d93e4c8ff7b274
Alex Plescan <alexpls@fastmail.com>
2025-12-18 16:57:42 +1000

    nvim: templ lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -9,6 +9,7 @@ return {
       
       vim.lsp.enable("ruby_lsp")
       vim.lsp.enable("gopls")
+      vim.lsp.enable("templ")
 
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("UserLspConfig", {}),
