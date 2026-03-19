6eaa3fd1d550e2a75be129ab90fbff1d9af9add1
Alex Plescan <alexpls@fastmail.com>
2026-03-19 18:41:16 +1000

    nvim format on save
    ... again. I'm not sure why I turned this off in the first place?

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -6,7 +6,7 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
-      
+
       vim.lsp.enable("ruby_lsp")
       vim.lsp.enable("gopls")
       vim.lsp.enable("templ")
@@ -14,8 +14,24 @@ return {
       vim.api.nvim_create_autocmd("LspAttach", {
         group = vim.api.nvim_create_augroup("UserLspConfig", {}),
         callback = function(ev)
+          local client = vim.lsp.get_client_by_id(ev.data.client_id)
+
           vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf })
           vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = ev.buf })
+
+          if client:supports_method("textDocument/formatting") then
+            vim.keymap.set("n", "<leader>cf", function()
+              vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 500 })
+            end, { buffer = ev.buf, desc = "Format buffer" })
+
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = vim.api.nvim_create_augroup("LspFormatOnSave", { clear = false }),
+              buffer = ev.buf,
+              callback = function()
+                vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 500 })
+              end,
+            })
+          end
         end,
       })
     end
