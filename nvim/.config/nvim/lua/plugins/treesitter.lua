db04f9a069c78494585b6f22ec8e7d8b52dfb0c1
Alex Plescan <alexpls@fastmail.com>
2026-05-05 08:29:25 +1000

    nvim fix treesitter startup

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -6,13 +6,15 @@ return {
     lazy = false,
     build = ":TSUpdate",
     config = function()
-      require("nvim-treesitter").install({
+      local filetypes = {
         "lua", "vim", "vimdoc", "javascript", "html", "css",
-        "typescript", "ruby", "go", "markdown", "yaml",
-      })
+        "typescript", "ruby", "go", "markdown", "yaml", "templ",
+      }
+
+      require("nvim-treesitter").install(filetypes)
 
       vim.api.nvim_create_autocmd('FileType', {
-        pattern = { '<filetype>' },
+        pattern = filetypes,
         callback = function() vim.treesitter.start() end,
       })
     end,
